cfe2d741cd1be56fb3fc0ff1bb896bbcb6651bc3
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-12 01:10:59 -0800

    Add rpsResult function

--- script.js
+++ script.js
@@ -8,4 +8,29 @@ function computerPlay(){
     else return 'Scissors';
 }
 
-console.log(computerPlay());
+function rpsResult(playerChoice, computerChoice){
+    playerChoice= firstCapitalized(playerChoice);
+    if(playerChoice === computerChoice) return `You Tied! ${playerChoice} Ties ${computerChoice}`;
+    else if(playerChoice === "Rock"){
+        if (computerChoice==="Scissors") return `You Win! ${playerChoice} beats ${computerChoice}`;
+        if (computerChoice==="Paper") return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+    else if(playerChoice === "Paper"){
+        if (computerChoice==="Rock") return `You Win! ${playerChoice} beats ${computerChoice}`;
+        if (computerChoice==="Scissors") return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+    else if(playerChoice === "Scissors"){
+        if (computerChoice==="Paper") return `You Win! ${playerChoice} beats ${computerChoice}`;
+        if (computerChoice==="Rock") return `You lose! ${computerChoice} beats ${playerChoice}`;
+    }
+
+}
+
+function firstCapitalized(str){
+    first = str.slice(0,1);
+    rest = str.slice(1);
+    first = first.toUpperCase();
+    rest = rest.toLowerCase();
+    return first+rest;
+}
+console.log(rpsResult('rOck', computerPlay()));
