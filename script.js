ba975015dc248eeeddefdc772455c7de2b4cac50
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-11 23:56:25 -0800

    Add computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //script.js
+
+function computerPlay(){
+    rand = Math.random();
+    console.log(rand);
+    if(rand <=.33) return 'Rock';
+    else if (rand >.33 && rand <=.66) return 'Paper';
+    else return 'Scissors';
+}
+
+console.log(computerPlay());
