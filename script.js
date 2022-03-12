092ad2fd1b4b8c0668930622e7e2342fbb720693
Daniel <100245905+birchbeak@users.noreply.github.com>
2022-03-12 15:19:17 -0800

    Create game function

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 //script.js
 
+game();
+
+function game(){
+    console.log("Ready to play Rock Paper Scissors?");
+    let playerScore=0;
+    let compScore=0;
+
+    for(let i=0; i<5; i++){
+        let choice= prompt("Choose!", "");
+        if(choice === null){
+            console.log("Cancelled");
+            return 1;
+        }
+
+        let resultText= rpsResult(choice, computerPlay() );
+        let result = resultText.slice(4,5);
+        console.log(resultText);
+        
+        if (result === 'T') ;
+        else if(result === 'W') playerScore++;
+        else if(result === 'L') compScore++;
+        else{
+            i--;
+            console.log('Please Try Again');
+        }
+    }
+    
+    if(playerScore===compScore){
+        console.log(`The game tied ${playerScore} to ${compScore}`);
+    }
+    else if(playerScore>compScore){
+        console.log(`You won the game ${playerScore} to ${compScore}!`);
+    }
+    else{
+        console.log(`You lost the game ${playerScore} to ${compScore}`);
+    }
+}
+
 function computerPlay(){
     rand = Math.random();
     console.log(rand);
@@ -9,19 +47,24 @@ function computerPlay(){
 }
 
 function rpsResult(playerChoice, computerChoice){
-    playerChoice= firstCapitalized(playerChoice);
+    if (playerChoice !==null) playerChoice= firstCapitalized(playerChoice);
+    else return null;
+
     if(playerChoice === computerChoice) return `You Tied! ${playerChoice} Ties ${computerChoice}`;
     else if(playerChoice === "Rock"){
         if (computerChoice==="Scissors") return `You Win! ${playerChoice} beats ${computerChoice}`;
-        if (computerChoice==="Paper") return `You lose! ${computerChoice} beats ${playerChoice}`;
+        if (computerChoice==="Paper") return `You Lose! ${computerChoice} beats ${playerChoice}`;
     }
     else if(playerChoice === "Paper"){
         if (computerChoice==="Rock") return `You Win! ${playerChoice} beats ${computerChoice}`;
-        if (computerChoice==="Scissors") return `You lose! ${computerChoice} beats ${playerChoice}`;
+        if (computerChoice==="Scissors") return `You Lose! ${computerChoice} beats ${playerChoice}`;
     }
     else if(playerChoice === "Scissors"){
         if (computerChoice==="Paper") return `You Win! ${playerChoice} beats ${computerChoice}`;
-        if (computerChoice==="Rock") return `You lose! ${computerChoice} beats ${playerChoice}`;
+        if (computerChoice==="Rock") return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    }
+    else{
+        return `Choice not recognized.`;
     }
 
 }
@@ -33,4 +76,3 @@ function firstCapitalized(str){
     rest = rest.toLowerCase();
     return first+rest;
 }
-console.log(rpsResult('rOck', computerPlay()));
